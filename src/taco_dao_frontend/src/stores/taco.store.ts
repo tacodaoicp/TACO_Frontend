62efbe886a29a197258d05ca4256dda743c5b409
Eric Rose <75504635+ericrosedev@users.noreply.github.com>
2025-09-21 15:02:13 -0700

    Removes double definition
    - removes second definition of portfolioArchiveCanisterId in store

--- src/taco_dao_frontend/src/stores/taco.store.ts
+++ src/taco_dao_frontend/src/stores/taco.store.ts
@@ -1239,15 +1239,6 @@ export const useTacoStore = defineStore('taco', () => {
         }        
         return 'cjkka-gyaaa-aaaan-qz5kq-cai'; // local canisterId
     }
-    const portfolioArchiveCanisterId = () => {
-        switch (process.env.DFX_NETWORK) {
-            case "ic":
-                return process.env.CANISTER_ID_PORTFOLIO_ARCHIVE_IC || 'bl7x7-wiaaa-aaaan-qz5bq-cai';
-            case "staging":
-                return process.env.CANISTER_ID_PORTFOLIO_ARCHIVE_STAGING || 'lrekt-uaaaa-aaaan-qz4ya-cai';
-        }        
-        return 'lrekt-uaaaa-aaaan-qz4ya-cai'; // local canisterId
-    }
     const alarmCanisterId = () => {
         switch (process.env.DFX_NETWORK) {
             case "ic":
